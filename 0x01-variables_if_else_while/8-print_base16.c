8c898e756e46bf78f915449d12b3a477cb8b71d4
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-17 09:44:19 -0700

    fix betty in ex 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,18 +7,18 @@
 /**
  * main - Entry point
  * this main function prints prints all the numbers of base 16 in lowercase
- * Return: Always an output messaage then  0 (success)
+ * Return: Always an output messaage then 0 (success)
  */
 int main(void)
 {
 int i;
 char c;
 c = 'a';
-for(i=48; i < 58; ++i)
+for (i = 48; i < 58; ++i)
 {
 putchar(i);
 }
-for(i=0; i < 6; ++i)
+for (i = 0; i < 6; ++i)
 {
 putchar(c);
 c++;

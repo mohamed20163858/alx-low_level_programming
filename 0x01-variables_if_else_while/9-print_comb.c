91c886b49ddb4264b4a855aac50a7ee642ec6105
Mohamed Abd Elmohsen Saleh <mohamed20163858@gmail.com>
2023-03-17 17:24:02 +0200

    Update 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * this main function prints all possible combinations of single-digit numbers.
+ * Return: Always an output messaage then  0 (success)
+ */
+int main(void)
+{
+int i;
+for(i=48; i < 58; ++i)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

01d4295479723a6cab15687e10f280a77b8f58ef
Mohamed Abd Elmohsen Saleh <mohamed20163858@gmail.com>
2023-03-17 16:59:29 +0200

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ * this main function print the alphapt from a to z except q and e
+ * Return: Always an output messaage then  0 (success)
+ */
+int main(void)
+{
+char c;
+int i;
+c = 'a';
+for(i=0; i < 26; ++i)
+{
+if (c == 'q' || c == 'e')
+{
+c++;
+continue;
+}
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}

504f0e19c8e0e8af9ce1356d26ca549d89c2733f
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 14:12:00 -0700

    Add to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,10 @@ void print_alphabet(void);
  * print_alphabet - print lowercase alphabet 10 times
  */
 void print_alphabet_x10(void);
+
+/**
+ * _islower - check for an input 
+ * @c: as an input sample of type integer
+ * Return: int
+ */
+int _islower(int c);

75bd3016c461793ef2ad0c3081dc7691a28993ed
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 13:46:56 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: print alphaet from a to z 10 times
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; ++i)
+{
+print_alphabet();
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,7 @@
  */
 
 void print_alphabet(void);
+/**
+ * print_alphabet - print lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void);

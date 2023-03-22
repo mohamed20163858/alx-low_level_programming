146d96ccd1595a9922f98be37bd193a3ebbd8e7a
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 16:19:28 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - a function that add 2 given values
+ * @a: the first argument
+ * @b: the second argument
+ * Return: sum of the 2 given values
+ */
+int add(int a, int b)
+{
+return (a + b);
+}

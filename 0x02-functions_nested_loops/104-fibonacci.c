c475181a46a0036ac5ea63247689afb9515e7c94
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 18:18:42 -0700

    task 16 test

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int i;
+long int fi, fi_plus_1, temp;
+fi = 1;
+fi_plus_1 = 2;
+printf("%ld, %ld", fi, fi_plus_1);
+for (i = 0; i < 96; ++i)
+{
+temp = fi + fi_plus_1;
+fi = fi_plus_1;
+fi_plus_1 = temp;
+printf(", %ld", temp);
+}
+printf("\n");
+return (0);
+}

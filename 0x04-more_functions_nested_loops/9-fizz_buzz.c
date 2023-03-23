a760fc61b16e83aa5793e226b0d45a674af785ca
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-23 08:16:39 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 means success
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; ++i)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
+}

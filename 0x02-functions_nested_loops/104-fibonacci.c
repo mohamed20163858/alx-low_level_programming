464783b75efe11d384fe32a74796536df7bf363c
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 19:03:45 -0700

    f

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,6 +7,9 @@ int main(void)
 {
 int i;
 long int fi, fi_plus_1, temp;
+long fi_part_1, fi_part_2, fi_plus_1_part_1
+long fi_plus_1_part_2, overflow, temp_part_2;
+long num = 10000000000;
 fi = 1;
 fi_plus_1 = 2;
 printf("%ld, %ld", fi, fi_plus_1);
@@ -16,6 +19,21 @@ temp = fi + fi_plus_1;
 fi = fi_plus_1;
 fi_plus_1 = temp;
 printf(", %ld", temp);
+}
+for (i = 89; i < 96; ++i)
+{
+fi_part_1 = fi % num;
+fi_part_2 =  fi / num;
+fi_plus_1_part_1 = fi_plus_1 % num;
+fi_plus_1_part_2 = fi_plus_1 / num;
+overflow = (fi_part_1 + fi_plus_1_part_1) / num;
+temp = (fi_part_1 + fi_plus_1_part_1) % num;
+temp_part_2 = fi_part_2 + fi_plus_1_part_2 + overflow;
+fi_part_1 = fi_plus_1_part_1;
+fi_part_2 = fi_plus_1_part_2;
+fi_plus_1_part_1 = temp;
+fi_plus_1_part_2 = temp_part_2;
+printf(", %ld%ld", temp, temp_part_2);
 }
 printf("\n");
 return (0);

b6c5c06cca2c05f515349f243f682f244b8ff721
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-23 06:45:05 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * mul - return the multiplication of the given two values
+ * @a: the first input
+ * @b: the second input
+ * Return: return the multiplication of the given two values
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,10 @@ int _isupper(int c);
  * Return: 1 if the given input is digit and 0 otherwise
  */
 int _isdigit(int c);
+/**
+ * mul - return the multiplication of the given two values
+ * @a: the first input
+ * @b: the second input
+ * Return: return the multiplication of the given two values
+ */
+int mul(int a, int b);

0395774c6eff65e094fb15e0b625e6f1d916a508
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 18:33:29 -0700

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ long int fi, fi_plus_1, temp;
 fi = 1;
 fi_plus_1 = 2;
 printf("%ld, %ld", fi, fi_plus_1);
-for (i = 0; i < 95; ++i)
+for (i = 0; i < 89; ++i)
 {
 temp = fi + fi_plus_1;
 fi = fi_plus_1;

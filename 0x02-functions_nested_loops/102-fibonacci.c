d8cd59e0a9c4f66e9683c671694d38847a031f9a
mohamed20163858 <mohamed20163858@gmail.com>
2023-03-22 17:42:10 -0700

    f

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@ int i;
 long int fi, fi_plus_1, temp;
 fi = 1;
 fi_plus_1 = 2;
-printf("%d, %d", fi, fi_plus_1);
+printf("%ld, %ld", fi, fi_plus_1);
 for (i = 0; i < 48; ++i)
 {
 temp = fi + fi_plus_1;

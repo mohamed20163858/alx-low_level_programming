2722daad46951eeeb62b35dd86e321f3f644d35f
mohamed20163858 <mohamed20163858@gmail.com>
2023-04-04 02:11:52 -0700

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,15 @@
  * @n: the input pointerto integer
  */
 void reset_to_98(int *n);
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: the first argument
+ * @b: the second argument
+ */
+void swap_int(int *a, int *b);
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the pointer to char
+ * Return: the length of string
+ */
+int _strlen(char *s);

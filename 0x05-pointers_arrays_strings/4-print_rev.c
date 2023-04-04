cc80f1898d5fbb328d2b3f65eb32017ed98765cb
mohamed20163858 <mohamed20163858@gmail.com>
2023-04-04 08:55:57 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,22 @@
+#include "main.h"
 /**
- * print_rev -  a function that prints every other character of a string, starting with the first character, followed by a new line.
+ * print_rev -  a function that prints every other character of a string
+ * , starting with the first character, followed by a new line.
  * @s: the pointer to char
  */
 void print_rev(char *s)
 {
+	int i;
+	int j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++i;
+	}
+	for (j = i; j >= 0; --j)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }

6c2d7412624fcd6cac246159692549c2a6c94204
mohamed20163858 <mohamed20163858@gmail.com>
2023-04-05 10:37:46 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,4 +4,19 @@
  */
 void rev_string(char *s)
 {
+	int i;
+	int j;
+	char temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++i;
+	}
+	for (j = 0; j <= i / 2; ++j)
+	{
+		temp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = temp;
+	}
 }

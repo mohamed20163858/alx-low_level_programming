6079d0c6f07d46c1f7a5be47a2f1bab0428ce5f2
mohamed20163858 <mohamed20163858@gmail.com>
2023-04-09 09:24:44 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,29 +1,31 @@
 #include "main.h"
+#define NULL 0
 /**
- * _strpbrk - return pointer to byte in s that matches a byte in accepted target
+ * _strpbrk - return pointer to byte in s that matches
+ * a byte in accepted target
  * @s: string to search
  * @accept: target matches
  * Return: pointer to index of string at first occurence
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i;
-    int j;
-    
-    i = 0;
-    j = 0;
-    while(s[i] != '\0')
-    {
-        while(accept[j] != '\0')
-        {
-            if (s[i] == accept[j])
-            {
-                return (s + i);
-            }
-            ++j;
-        }
-        ++i;
-        j = 0;
-    }
-    return (NULL);
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			++j;
+		}
+		++i;
+		j = 0;
+	}
+	return (NULL);
 }
